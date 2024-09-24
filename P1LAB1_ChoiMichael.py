96d4e80a10f24ff6e997bfe8ea9d8442e53cdd0a
lordyun <mryunchoi@gmail.com>
2024-09-24 14:21:19 -0400

    First upload

---
+++ P1LAB1_ChoiMichael.py
@@ -0,0 +1,13 @@
+# Michael Choi
+# 9/24/244
+# P1LAB1
+# get input from the user and display output
+
+# get first name from user
+fname = input("Enter your first name: ")
+
+# get last name from user
+lname = input("Enter your last name: ")
+
+# display input to use
+print("Hello,", fname, lname + "! Welcome to CTI-110")
